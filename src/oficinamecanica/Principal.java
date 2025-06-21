2daecb7c568ab741d64c4a38df11b622f7590860
camila-barbosa <barbosacamilla8@gmail.com>
2025-06-21 18:32:51 -0300

    branch teste

--- src/oficinamecanica/Principal.java
+++ src/oficinamecanica/Principal.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package oficinamecanica; // Seu pacote principal, que contém a classe Main/Principal
+package oficinamecanica;
 
 import java.util.Scanner;
 import models.Usuario;
